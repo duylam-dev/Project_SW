5f1062e895ee724f9a82178742565134dc99bb87
lam <lam2012003az@gmail.com>
2024-10-18 23:31:19 +0700

    init project be

---
+++ src/backend/walletApi/src/main/java/it3180/team19/walletapi/WalletApiApplication.java
@@ -0,0 +1,13 @@
+package it3180.team19.walletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApiApplication.class, args);
+	}
+
+}
---
+++ src/backend/walletApi/src/test/java/it3180/team19/walletapi/WalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it3180.team19.walletapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
